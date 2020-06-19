a23025bec29cf9fd7279bbbbfed26346dcae7628
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-06-19 14:38:44 +0200

    Clean js code

--- docker/image-musician/src/app.js
+++ docker/image-musician/src/app.js
@@ -1,4 +1,3 @@
-console.log(process.argv);
 var protocol = require('./musician_protocol')
 const dgram = require('dgram');
 const { v4: uuidv4 } = require('uuid');
@@ -16,22 +15,28 @@ const instrumentMap = {
 
 function Musician(instrumentRequest){
 
+    // Create the unique uuid
     this.uuid = uuidv4();
 
     Musician.prototype.update = function(){
         
         var time = new Date()
+
+        // Create data to send
         var data = {
             uuid : this.uuid,
             instrument : instrumentRequest,
             sound : instrumentMap[instrumentRequest],
             activeSince : time.toJSON()
         }
+
+        // Transform the date into JSON format
         payload = JSON.stringify(data);
 
-
+        // Put the payload in a buffer
         message = new Buffer(payload);
 
+        // Send the message to multicast addr
         s.send(message, 0, message.length, 
             protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS, 
             function(err, bytes){
@@ -39,11 +44,11 @@ function Musician(instrumentRequest){
             });
     }
 
+    // Do this every second
     setInterval(this.update.bind(this),1000);
 }
 
 
 var m = new Musician(process.argv[2]);
 
-//var m = new Musician('piano')
 
